f46f81b55a1671aa8fdb91d5eecc4fe33579c096
Alma G <almarcianita@gmail.com>
2018-03-15 13:11:29 -0600

    Flash

---
+++ src/pages/home/home.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import {Flashlight} from '@ionic-native/flashlight';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController, private flashlight: Flashlight) {
+
+    function toggleFlash(){
+      this.flashlight.toggle();
+    }
+   
+
+  }
+
+}
